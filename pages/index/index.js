33ce4a93d6f11332f8dc921254d81b14006fb31a
Jujiu <q2335185714@qq.com>
2018-01-21 22:48:26 +0800

    setData设置页面数据

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,8 +4,14 @@ Page({
   data: {
     greeting: app.globalData.greeting
   },
+  greet(greeting) {
+    return greeting[Math.floor(Math.random() * greeting.length)]
+  },
   onLoad() {
     console.log('页面加载')
+    this.setData({
+      greeting: this.greet(app.globalData.greeting)
+    })
   },
   onShow() {
     console.log('页面显示')
