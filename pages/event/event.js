dc9099848ea87d1a019d0856aa0278a829bc0eba
Jujiu <q2335185714@qq.com>
2018-01-21 21:32:53 +0800

    页面生命周期函数示例

--- pages/event/event.js
+++ pages/event/event.js
@@ -1,1 +1,17 @@
-Page({})
+Page({
+  onLoad() {
+    console.log('页面加载')
+  },
+  onShow() {
+    console.log('页面显示')
+  },
+  onReady() {
+    console.log('页面首次渲染后执行')
+  },
+  onHide() {
+    console.log('页面隐藏')
+  },
+  onUnload() {
+    console.log('页面卸载')
+  }
+})
