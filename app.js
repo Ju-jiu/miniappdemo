59c19c29303ac5778738461a7ddf9dc961a0a649
Jujiu <q2335185714@qq.com>
2018-01-21 17:09:53 +0800

    生命周期函数示例

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-App({})
+App({
+  onLaunch(options) {
+    console.log('初始化。小程序初始化以后被执行')
+    console.log(options)
+  },
+  onShow(options) {
+    console.log('显示。小程序启动以后，从后台进入到前台后被执行')
+    console.log(options)
+  },
+  onHide() {
+    console.log('隐藏。小程序从前台进入到后台后被执行')
+  },
+  onError(error) {
+    console.log('error')
+  }
+})
